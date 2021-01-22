a17b497d74bbae80155c160eb30a324fa13b91bf
tigaotoy33 <77850502+tigaotoy33@users.noreply.github.com>
2021-01-22 23:50:38 +0700

    Add files via upload

---
+++ socks4 2.php
@@ -0,0 +1,17 @@
+<?php
+$directory = '.';
+$url  = 'https://api.proxyscrape.com/v2/?request=getproxies&protocol=socks4&timeout=10000&country=all';
+$path = $directory.'/socks4 2.txt';
+$fp = fopen($path, 'w');
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_FILE, $fp);
+$data = curl_exec($ch);
+if($data){
+ echo 'File backup sudah diterima...';
+}
+else{
+ echo 'Yah... Gagal Mengambil File';
+}
+curl_close($ch);
+fclose($fp);
+?>
